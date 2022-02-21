09a3d6305d6a50164528dc9cf68753709a8c0313
sanderverbeken <r0747326@ucll.be>
2022-02-21 16:23:15 +0100

    contiuing on the script

--- script/Setup_freebsd_bhyve.sh
+++ script/Setup_freebsd_bhyve.sh
@@ -23,6 +23,8 @@ zfs create zroot/vm
 sysrc vm_enable="YES"
 sysrc vm_dir="zfs:zroot/vm"
 
+cd /zroot/vm
+
 vm init
 
 #kopieer de al bestaande templates naar de map foor bhyve om te gebruiken.
@@ -33,8 +35,24 @@ mkdir -p zroot/vm/.templates
 vm switch create public
 vm switch add public le0
 
-cd /zroot/vm/.iso
-vm iso https://download.freebsd.org/ftp/releases/ISO-IMAGES/11.2/FreeBSD-11.2-RELEASE-amd64-bootonly.iso
+cd /usr/local/share/examples/vm-bhyve
+
+cp windows.conf /zroot/vm/.templates/windows10.conf
+cp windows.conf /zroot/vm/.templates/windows2022.conf
+cp ubuntu.conf /zroot/vm/.templates/mint.conf
+cp freebsd-zvol.conf /zroot/vm/.templates/freebsd.conf
+cp freebsd-zvol.conf /zroot/vm/.templates/freebsd.conf.bac
+
+cd zroot/vm
+
+vm create -t ubuntu -s 8G ubuntu
+vm iso https://releases.ubuntu.com/20.04/ubuntu-20.04.3-live-server-amd64.iso
+vm install ubuntu .iso/ubuntu-20.04.3-live-server-amd64.iso
+
+
+
+#cd /zroot/vm/.iso
+#vm iso https://download.freebsd.org/ftp/releases/ISO-IMAGES/11.2/FreeBSD-11.2-RELEASE-amd64-bootonly.iso
 
 #vm create myguest
 #vm install [-f] myguest FreeBSD-11.2-RELEASE-amd64-bootonly.iso
