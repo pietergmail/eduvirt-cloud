99e7913927e650ebd13fa6f4ab10601ab08b5313
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2022-02-25 15:03:15 +0100

    update script

--- web-backend/nomad/script.sh
+++ web-backend/nomad/script.sh
@@ -9,10 +9,4 @@ name=$user"@"$templateName
 
 vm clone $templateName $name
 
-<<<<<<< HEAD
-vm start $name
-
 sed -i 's/public/$vak/' /home/t/zroot/$name.conf
-=======
-sed -i 's/public/$vak/' /home/t/zroot/$name.conf
->>>>>>> 7d30755e434b7df0d0a585ce3ef9f2e8da5f1f23
