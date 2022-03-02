cfd9c8b463b4d57cc4ed709bb064574f1a5f15fc
pietergmail <herremanspieter@gmail.com>
2022-03-02 10:56:41 +0100

    lol this works is guess:

--- web-backend/nomad/script.sh
+++ web-backend/nomad/script.sh
@@ -6,10 +6,29 @@ vak=$3
 
 name=$user"_"$templateName
 
+if (vm list | grep $name)
+then
+    if($name | grep "_")
+    then
+        lastchar=${name: -1} #get last char
+        name=${name:: -1} #remove last char from name
+        name+=$((lastchar + 1)) #add new last char to name
+    else
+        name+=_1
+    fi
+fi
+
 vm clone $templateName $name
 
 cd /zroot/vm/$name
 
-sed -i 's/public/$vak/' $name.conf
+if(vm switch list | grep $vak)
+then
+    sed -i "s/public/$vak/" "$name.conf"
+else
+    vm switch create $vak
+    #vm switch add $vak em0
+    sed -i "s/public/$vak/" "$name.conf"
+fi
 
 vm start $name
