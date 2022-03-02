2faa3cbe27d2a2145a3f3151b6c609ab32485910
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2022-03-02 16:01:06 +0100

    changes for create machine

--- web-backend/routes/RoutingEndpoint.js
+++ web-backend/routes/RoutingEndpoint.js
@@ -100,7 +100,7 @@ const createMachine = (req, res) => {
     
 
     // script to create machine
-    exec('nomad job run -var=\"templateName=' + req.body.templateName + '\" -var=\"userName=' + req.body.username + '\" -var=\"courseName=' + req.body.courseName + '\" /home/t/eduvirt-cloud/web-backend/nomad/job.nomad',
+    exec('nomad job run -var=\"templateName=' + req.body.mName + '\" -var=\"userName=' + req.body.username + '\" -var=\"courseName=' + req.body.mCourse + '\" /home/t/eduvirt-cloud/web-backend/nomad/job.nomad',
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
