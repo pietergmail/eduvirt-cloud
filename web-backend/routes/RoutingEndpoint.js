66b5bc1417cd187833fa440017e43637ef08583c
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2022-03-03 10:38:28 +0100

    changes for exec

--- web-backend/routes/RoutingEndpoint.js
+++ web-backend/routes/RoutingEndpoint.js
@@ -72,15 +72,16 @@ const getMachinesOfUser = (req, res) => {
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
       const machines = []
-      stdout = "FreeBSD bhyveloader 2 8 192.0.0.1 running systeembeheer" + "\n" + "pepegaOS pepeloader 2 16 192.0.0.1 stopped systeembeheer"
+    //   stdout = "FreeBSD bhyveloader 2 8 192.0.0.1 running systeembeheer" + "    " + "pepegaOS pepeloader 2 16 192.0.0.1 stopped systeembeheer"
+      
 
     //   get every line of stdout and put every word in a new machine
-        stdout.split("\n").forEach(line => {
+        stdout.split("  ").forEach(line => {
             const words = line.split(" ");
             const machine = new Machine(words[0], words[1], words[2], words[3], words[4], words[5], words[6]);
             machines.push(machine);
         });
-      
+        
       res.send(machines);
       if (error !== null) {
         console.log('exec error: ' + error);
