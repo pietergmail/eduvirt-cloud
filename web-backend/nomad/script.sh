f05b85143cc2bfd7582620d4871b972ecbfe2b46
Matthias Veelaert <matthias.veelaert@student.ucll.be>
2022-03-03 12:01:06 +0100

    script works

--- web-backend/nomad/script.sh
+++ web-backend/nomad/script.sh
@@ -5,24 +5,33 @@ user=$2
 vak=$3
 
 name=$user"_"$templateName
- 
 if (vm list | grep -q $name)
 then
     if (vm list | grep -q $name-);
     then
 	lastName=$(vm list | grep Ubuntu | tail -1 | awk '{print $1}')
+	VncPort=$(echo | cat /zroot/vm/$lastName/$lastName.conf | grep 'graphics_port' | grep -o "[0-9][0-9][0-9][0-9]")
+	echo $VncPort
 	lastchar=${lastName: -1} #get last char
-	echo "$lastchar"
         name=${lastName:: -1} #remove last char from name
         newName="${name}$((lastchar + 1))" #add new last char to name
-	echo "$newName"
 	name=$newName
+	vm clone $templateName $name
+	sed -i .bak "s/$VncPort/$((VncPort + 1))/" "/zroot/vm/$name/$name.conf"
+	VncPort=$((VncPort + 1))
+	echo $VncPort
+	rm /zroot/vm/$name/$name.conf.bak
     else
+	VncPort=$(echo | cat /zroot/vm/$name/$name.conf | grep 'graphics_port' | grep -o "[0-9][0-9][0-9][0-9]")
+	echo $VncPort
         name="${name}-1"
+	vm clone $templateName $name
+	sed -i .bak "s/$VncPort/$((VncPort + 1))/" "/zroot/vm/$name/$name.conf"
+	rm /zroot/vm/$name/$name.conf.bak
     fi
+else 
+	vm clone $templateName $name
 fi
-
-vm clone $templateName $name
 
 cd /zroot/vm/$name
 
