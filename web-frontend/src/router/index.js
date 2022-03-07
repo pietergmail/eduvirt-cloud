8dc3b62ca2c130f55b4688096d864aef560942ba
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2022-03-07 10:26:14 +0100

    rommel removal

--- web-frontend/src/router/index.js
+++ web-frontend/src/router/index.js
@@ -43,14 +43,6 @@ const routes = [
     component: Machines,
     // beforeEnter: authGuard
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
