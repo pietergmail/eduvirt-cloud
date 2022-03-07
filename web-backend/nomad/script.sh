fbb74a640a8d263d363486a65ec9ec8425cbe1e5
Matthias Veelaert <matthias.veelaert@student.ucll.be>
2022-03-07 14:50:21 +0100

    changes for vnc en naamgeving

--- web-backend/nomad/script.sh
+++ web-backend/nomad/script.sh
@@ -4,29 +4,30 @@ templateName=$1
 user=$2
 vak=$3
 
-name=$user"_"$templateName
+name=$user"_"$templateName"_Server1"
+VncPort=$(echo | cat /zroot/vm/$templateName/$templateName.conf | grep 'graphics_port' | grep -o "[0-9][0-9][0-9][0-9]")
 
 if (vm list | grep -q $name);
 then
     if (vm list | grep -q $name-);
     then
 	lastName=$(vm list | grep Ubuntu | tail -1 | awk '{print $1}')
-	VncPort=$(echo | cat /zroot/vm/$lastName/$lastName.conf | grep 'graphics_port' | grep -o "[0-9][0-9][0-9][0-9]")
 	lastchar=${lastName: -1} #get last char
         name=${lastName:: -1} #remove last char from name
         newName="${name}$((lastchar + 1))" #add new last char to name
 	name=$newName
+	sed -i .bak "s/4901/$((VncPort + 1))/" "/zroot/vm/$templateName/$templateName.conf"
+	rm /zroot/vm/$templateName/$templateName.conf.bak
 	vm clone $templateName $name
-	sed -i .bak "s/$VncPort/$((VncPort + 1))/" "/zroot/vm/$name/$name.conf"
-	rm /zroot/vm/$name/$name.conf.bak
     else
-	VncPort=$(echo | cat /zroot/vm/$name/$name.conf | grep 'graphics_port' | grep -o "[0-9][0-9][0-9][0-9]")
         name="${name}-1"
+	sed -i .bak "s/$VncPort/$((VncPort + 1))/" "/zroot/vm/$templateName/$templateName.conf"
+	rm /zroot/vm/$templateName/$templateName.conf.bak
 	vm clone $templateName $name
-	sed -i .bak "s/$VncPort/$((VncPort + 1))/" "/zroot/vm/$name/$name.conf"
-	rm /zroot/vm/$name/$name.conf.bak
     fi
 else 
+	sed -i .bak "s/$VncPort/$((VncPort + 1))/" "/zroot/vm/$templateName/$templateName.conf"
+        rm /zroot/vm/$templateName/$templateName.conf.bak
 	vm clone $templateName $name
 fi
 
